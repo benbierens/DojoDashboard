dccc25981e4846c9d77f094f47b57dfb49f4af1b
Ben Bierens <ben.bierens@philips.com>
2021-05-05 11:13:12 +0200

    how to async

--- dojodash/lib/screens/home/home_screen.dart
+++ dojodash/lib/screens/home/home_screen.dart
@@ -10,43 +10,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          IconButton(icon: Icon(Icons.home), onPressed: () => context.yeet('/')),
-          IconButton(icon: Icon(Icons.person), onPressed: () => context.yeet('/maze'))
-        ]
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+        appBar: AppBar(title: Text(widget.title), actions: [
+          IconButton(
+              icon: Icon(Icons.home), onPressed: () => context.yeet('/')),
+          IconButton(
+              icon: Icon(Icons.person), onPressed: () => context.yeet('/maze'))
+        ]),
+        body: FutureBuilder(
+            future: Future.delayed(Duration(seconds: 3), () => 'Cheese'),
+            builder: (_, data) {
+              if (data.hasData) {
+                return Text('It was: ${data.data}');
+              }
+              return Text('Bussje komt zo');
+            }));
   }
 }
