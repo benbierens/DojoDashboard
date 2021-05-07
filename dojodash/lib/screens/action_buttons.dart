c64dd4c0956aff96329356e0fc57a577ccbe8b43
Ben Bierens <ben.bierens@philips.com>
2021-05-07 08:35:39 +0200

    extracts action buttons

---
+++ dojodash/lib/screens/action_buttons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:yeet/yeet.dart';
+
+List<Widget> getActionButtons(BuildContext context) {
+  return [
+    IconButton(icon: Icon(Icons.home), onPressed: () => context.yeet('/')),
+    IconButton(
+        icon: Icon(Icons.person), onPressed: () => context.yeet('/maze')),
+    IconButton(icon: Icon(Icons.vpn_key), onPressed: () => context.yeet('/key')),
+  ];
+}
